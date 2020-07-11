0d0bf0ec5a3b89ae01c89447ec8619a1e8f8f447
kamleshsilag <kamleshsilag42@gmail.com>
2020-07-11 11:54:55 +0530

    Compute circle area test Using Test Driven Development

--- src/test/java/io/kamlesh/MathUtilsTest.java
+++ src/test/java/io/kamlesh/MathUtilsTest.java
@@ -7,10 +7,16 @@ import static org.junit.jupiter.api.Assertions.*;
 public class MathUtilsTest {
 
     @Test
-    void test(){
+    void testAdd(){
         MathUtils mathUtils = new MathUtils();
         int expected = 2;
         int actual = mathUtils.add(1,1);
-        assertEquals(expected,actual);
+        assertEquals(expected,actual, "The add method should add two numbers");
+    }
+
+    @Test
+    void testcomputeCircleArea(){
+        MathUtils mathUtils = new MathUtils();
+        assertEquals(314.1592653589793, mathUtils.computeCircleArea(10), "Should return right circle area");
     }
 }
