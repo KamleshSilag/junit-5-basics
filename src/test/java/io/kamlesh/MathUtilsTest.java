7dcf3322a36a30fb777df42300b5248b12d68c39
kamleshsilag <kamleshsilag42@gmail.com>
2020-07-11 11:32:40 +0530

    Adding test for add method

--- src/test/java/io/kamlesh/MathUtilsTest.java
+++ src/test/java/io/kamlesh/MathUtilsTest.java
@@ -2,12 +2,15 @@ package io.kamlesh;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MathUtilsTest {
 
     @Test
     void test(){
-        fail("not yet implemented");
+        MathUtils mathUtils = new MathUtils();
+        int expected = 2;
+        int actual = mathUtils.add(1,1);
+        assertEquals(expected,actual);
     }
 }
